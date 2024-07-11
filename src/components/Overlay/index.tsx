e1058ea8a87003953339f0242f9238e7cb107197
Victor Brilistyi <victor.b@theevenstar.net>
2024-07-11 17:45:39 +0300

    refactored ui & html

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,9 +1,9 @@
-import { Radio, Grid, RadioGroup, FormControlLabel, FormControl, InputLabel, SelectChangeEvent, MenuItem, Select } from "@mui/material";
+import { Box, Radio, Grid, RadioGroup, FormControlLabel, FormControl, InputLabel, SelectChangeEvent, MenuItem, Select, Button, Typography, FormLabel } from "@mui/material";
 import { EventHandler, useCallback, useEffect, useState } from "react";
-import Box from '@mui/material/Box';
 import { colors, state, updateTableCoverColor, updateTableHeight, updateTableMaterial } from "../../store";
 import { MaterialTypes, TableHeightTypes } from "../../helpers/types";
 import { useSnapshot } from "valtio";
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 
 
@@ -38,34 +38,45 @@ export const Overlay = () => {
         updateTableHeight(event.target.value as TableHeightTypes);
     }, []);
 
-    // const [age, setAge] = useState('');
 
     const handleChange = (event: SelectChangeEvent) => {
-    //   setAge(event.target.value);
       updateTableMaterial(event.target.value as MaterialTypes);
     };
 
     return <>
         <div id="overlay">
             <div className="overlayHeader">
-                <button onClick={showHide}> Roll up </button>
-                <button> Download </button>
+                <Button 
+                    variant="outlined" 
+                    startIcon={<CameraAltIcon />}
+                >
+                    Download
+                </Button>
             </div>
             <div className="overlayContent"> 
-                <Box component="div" >
-                    <RadioGroup
-                        aria-labelledby="demo-radio-buttons-group-label"
-                        defaultValue={TableHeightTypes.default}
-                        name="radio-buttons-group"
-                        onChange={handleRadio} 
-                        value={tableHeight}
-                    >
-                        <FormControlLabel value={TableHeightTypes.default} control={<Radio />} label="default" />
-                        <FormControlLabel value={TableHeightTypes.h1} control={<Radio />} label={TableHeightTypes.h1} />
-                        <FormControlLabel value={TableHeightTypes.h2} control={<Radio />} label={TableHeightTypes.h2} />
-                        <FormControlLabel value={TableHeightTypes.h3} control={<Radio />} label={TableHeightTypes.h3} />
-                    </RadioGroup>
-                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <Box 
+                    component="div"
+                    style={{
+                        display:'flex',
+                        flexDirection:'column'
+                    }}
+                >
+                    <FormControl variant="standard" >
+                        <FormLabel id="demo-simple-select-standard-label">Table height</FormLabel>
+                        <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue={TableHeightTypes.default}
+                            name="radio-buttons-group"
+                            onChange={handleRadio} 
+                            value={tableHeight}
+                        >
+                            <FormControlLabel value={TableHeightTypes.default} control={<Radio />} label="default" />
+                            <FormControlLabel value={TableHeightTypes.h1} control={<Radio />} label={TableHeightTypes.h1} />
+                            <FormControlLabel value={TableHeightTypes.h2} control={<Radio />} label={TableHeightTypes.h2} />
+                            <FormControlLabel value={TableHeightTypes.h3} control={<Radio />} label={TableHeightTypes.h3} />
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl variant="standard" style={{marginTop: '20px'}}>
                         <InputLabel id="demo-simple-select-standard-label">Materials</InputLabel>
                         <Select
                             labelId="demo-simple-select-standard-label"
@@ -79,8 +90,8 @@ export const Overlay = () => {
                             <em>default</em>
                         </MenuItem> */}
                         <MenuItem value={MaterialTypes.default}>default</MenuItem>
-                        <MenuItem value={MaterialTypes.v1}>v1</MenuItem>
-                        <MenuItem value={MaterialTypes.v2}>v2</MenuItem>
+                        <MenuItem value={MaterialTypes.v1}>Wood 1</MenuItem>
+                        <MenuItem value={MaterialTypes.v2}>Wood 2</MenuItem>
                         {/* <MenuItem value={30}>Thirty</MenuItem> */}
                         </Select>
                     </FormControl>
